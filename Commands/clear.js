b8d722b58c71518536b68f966cf5bf27ba7257a7
Patrick Hogan <phogan110@live.com>
2019-01-16 14:59:46 -0700

    modified clear command

--- Commands/clear.js
+++ Commands/clear.js
@@ -1,6 +1,20 @@
 const Discord = require("discord.js");
-// !clear <>
-module.exports.run = async (bot, message, args) => {};
+// !clear <amount>
+module.exports.run = async (bot, message, args) => {
+  // if message sender doesn't have permissions reply with message
+  if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    return message.reply(
+      "you do not have sufficient permissions to use that command."
+    );
+  // if no arg reply with text
+  if (!args[0]) return message.reply("you need to specify a number to delete");
+  // delete specified number of messages in channel then send message then delete message after 5s
+  message.channel.bulkDelete(args[0]).then(() => {
+    message.channel
+      .send(`Cleared ${args[0]} messages.`)
+      .then(msg => msg.delete(5000));
+  });
+};
 
 module.exports.help = {
   name: "clear"
