012c9f40af6806ef88621fb4250d703c454c18bc
Patrick Hogan <phogan110@live.com>
2019-01-16 11:14:43 -0700

    commented

--- Commands/cat.js
+++ Commands/cat.js
@@ -2,18 +2,22 @@ const Discord = require("discord.js");
 const superAgent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
+  // wait for superAgent to get url and call body with body
   let { body } = await superAgent.get(`http://aws.random.cat/meow`);
-
+  // call new Discord RichEmbed with catEmbed
   let catEmbed = new Discord.RichEmbed()
+    // set spine color to black
     .setColor("#000000")
+    // set title to "Cat"
     .setTitle("Cat")
+    // set image to img in body.file
     .setImage(body.file);
-
+  // call #photos-screenshots with catChannel
   let catChannel = message.guild.channels.find(`name`, "photos-screenshots");
-
+  // if no catChannel reply with text
   if (!catChannel)
     return message.reply("I couldn't find a channel for the cats");
-
+  // send catEmbed to catChannel
   catChannel.send(catEmbed);
 };
 
--- Commands/dog.js
+++ Commands/dog.js
@@ -2,18 +2,22 @@ const Discord = require("discord.js");
 const superAgent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
+  // wait for superAgent to get .json and call with body
   let { body } = await superAgent.get(`https://random.dog/woof.json`);
-
+  // call new Discord Richembed with dogEmbed
   let dogEmbed = new Discord.RichEmbed()
+    // set color to white
     .setColor("#ffffff")
+    // set title to "Dog"
     .setTitle("Dog")
+    // set image to url img
     .setImage(body.url);
-
+  // call photos-screenshots with dogChannel
   let dogChannel = message.guild.channels.find(`name`, "photos-screenshots");
-
+  // if no dogChannel reply with message
   if (!dogChannel)
     return message.reply("I couldn't find a channel for the dogs");
-
+  // send dogEmbed to dogChannel
   dogChannel.send(dogEmbed);
 };
 
