47c67913f480ae39fd22741e0dcce3b05b054189
Patrick Hogan <phogan110@live.com>
2019-01-16 15:05:40 -0700

    modified say command

--- Commands/say.js
+++ Commands/say.js
@@ -1,6 +1,18 @@
 const Discord = require("discord.js");
 // !say <text to say>
-module.exports.run = async (bot, message, args) => {};
+module.exports.run = async (bot, message, args) => {
+  // if message sender doesn't have permission to manage messages
+  if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    return message.reply(
+      "you don't have sufficient permissions to use this command."
+    );
+  // join args with spaces and call with botMessage
+  let botMessage = args.join(" ");
+  // delete message with command
+  message.delete().catch();
+  // send botMessage to channel cmd was sent in
+  message.channel.send(botMessage);
+};
 
 module.exports.help = {
   name: "say"
