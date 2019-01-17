ba1a3bb359163e6d001a1ca195bc956e8c8f8582
Patrick Hogan <phogan110@live.com>
2019-01-16 17:00:30 -0700

    created 'level.js'

---
+++ Commands/level.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const purple = botconfig.purple;
+const xp = require("../xp.json");
+//
+module.exports.run = async (bot, message, args) => {
+  //
+};
+
+module.exports.help = {
+  name: "level"
+};
