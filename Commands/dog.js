e6b9938f1e62d8428f3bf359f679e7ed8b6be966
Patrick Hogan <phogan110@live.com>
2019-01-22 22:34:26 -0700

    updated to a website that doesn't use gifs

--- Commands/dog.js
+++ Commands/dog.js
@@ -3,7 +3,9 @@ const superAgent = require("superagent");
 // !dog
 module.exports.run = async (bot, message, args) => {
   // wait for superAgent to get .json and call body with body
-  let { body } = await superAgent.get(`https://random.dog/woof.json`);
+  let { body } = await superAgent.get(
+    `https://dog.ceo/api/breeds/image/random`
+  );
   // call new Discord RichEmbed with dogEmbed
   let dogEmbed = new Discord.RichEmbed()
     // set color to white
@@ -11,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
     // set title to "Dog"
     .setTitle("Dog")
     // set image to url img
-    .setImage(body.url);
+    .setImage(body.message);
   // call photos-screenshots with dogChannel
   let dogChannel = message.guild.channels.find(
     val => val.name === "photos-screenshots"
