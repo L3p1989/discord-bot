f6eeae0d31dc7dd054cdb9a6e0b1c68f2cee59d2
Patrick Hogan <phogan110@live.com>
2025-03-17 03:52:02 -0600

    The member.js file uses the updated role name from the configuration file

--- commands/member.js
+++ commands/member.js
@@ -1,5 +1,6 @@
 const { PermissionsBitField } = require('discord.js');
 const { handleError } = require('../utilities/error');
+const { roles } = require('../utilities/config');
 
 async function handleMemberCommand(message, client, member, memberRole) {
     try {
@@ -11,8 +12,8 @@ async function handleMemberCommand(message, client, member, memberRole) {
             if (botMember.roles.highest.comparePositionTo(memberRole) > 0) {
                 // Check if the user is the server owner
                 if (message.guild.ownerId === message.author.id) {
-                    message.reply(`OK ${member.displayName}, I have given you the Member role, but I cannot change your nickname as you are the server owner.`);
-                    await member.roles.add(memberRole); // Assign the "Member" role to the server owner
+                    message.reply(`OK ${member.displayName}, I have given you the ${roles.member} role, but I cannot change your nickname as you are the server owner.`);
+                    await member.roles.add(memberRole); // Assign the "Seraph" role to the server owner
                 } else {
                     // Ask for the in-game name
                     message.reply('Please provide your in-game name.');
@@ -22,17 +23,17 @@ async function handleMemberCommand(message, client, member, memberRole) {
                     const collected = await message.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] });
 
                     const inGameName = collected.first().content;
-                    const newNickname = `[Member] ${inGameName}`;
+                    const newNickname = `[${roles.member}] ${inGameName}`;
 
                     // Change the nickname
                     await member.setNickname(newNickname);
 
                     // Assign the role
                     await member.roles.add(memberRole);
-                    message.reply(`OK ${member.displayName}, I have given you the Member role and changed your nickname to ${newNickname}.`);
+                    message.reply(`OK ${member.displayName}, I have given you the ${roles.member} role and changed your nickname to ${newNickname}.`);
                 }
             } else {
-                message.reply(`Sorry ${member.displayName}, my role is not high enough to assign the Member role.`);
+                message.reply(`Sorry ${member.displayName}, my role is not high enough to assign the ${roles.member} role.`);
             }
         } else {
             message.reply(`Sorry ${member.displayName}, I don't have permission to manage roles or nicknames.`);
